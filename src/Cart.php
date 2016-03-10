27abde65a62d2d337d65460226f93847ee08d5f4
Bobo Chen <bobochen@kkbox.com>
2016-03-10 11:43:49 +0800

    Fix indent.

--- src/Cart.php
+++ src/Cart.php
@@ -17,13 +17,13 @@ class Cart
     {
         $group = array_chunk($this->data_source, $this->n);
         foreach ($group as $item) {
-            $ans = 0 ;
+            $ans = 0;
             foreach ($item as $it) {
                 $ans = $ans + (int)$it[$this->column];
             }
             $result[] = $ans;
         }
-        return implode(',',$result);
+        return implode(',', $result);
     }
 
     /**
